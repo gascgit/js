526cc2b9d6dd09a99d7aad5eee3841e0c90bc66e
gascgit <sergio@guidelinesad.com>
2016-11-11 09:38:40 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// IIFE - Immediately Invoked Function Expression
+  (function(yourcode) {
+
+    // The global jQuery object is passed as a parameter
+  	yourcode(window.jQuery, window, document);
+
+  }(function($, window, document) {
+
+    // The $ is now locally scoped 
+
+   // Listen for the jQuery ready event on the document
+   $(function() {
+
+     $("h2").append(" :after");
+     
+     // The DOM is ready!
+
+   });
+
+   // The rest of the code goes here!
+
+  }));
+
+(self.counter = function() {
+  var i = 0;
+
+  counter.get = function() {
+    return i;
+  };
+
+  counter.set = function(val) {
+    i = val;
+  };
+
+  counter.increment = function() {
+    return ++i;
+  };
+
+})();
+$("h2").append(counter.increment);
+
+for (var i = 0, j = 9; i <= j; i++, j--) {
+  $("h2").append("a[" + i + "][" + j + "]= " + a[i][j]);
+}
